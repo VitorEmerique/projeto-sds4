ba5ecd89071a3fd5d3e1945ffd67d25c2742e94d
VitorEmerique <vitort.emerique@hotmail.com>
2021-09-11 17:20:02 -0300

    Projeto sds4 final

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
-import { SaleSucess, SaleSum } from 'types/sale';
+import { SaleSucess } from 'types/sale';
 import { round } from 'utils/format';
 
 import { BASE_URL } from 'utils/requests';
@@ -39,7 +39,7 @@ const BarChart = () => {
             .then(response => {
                 const data = response.data as SaleSucess[];
                 const myLabels = data.map(x => x.sellerName);
-                const mySeries = data.map(x => round(100.0 * x.deals / x.visited,1));
+                const mySeries = data.map(x => round((100.0 * x.deals) / x.visited,1));
                 setChartData({
                     labels: {
                         categories: myLabels
